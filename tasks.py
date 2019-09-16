5f2244db8f82409ef0532c4bfba7acdf3b767184
x123069 <extern.lutz.collum@porsche.de>
2019-09-16 17:01:32 +0200

    update to the better

--- tasks.py
+++ tasks.py
@@ -1,126 +1,201 @@
 #!/usr/bin/env python
 
-import hashlib
 import json
 import os
 import re
+from time import sleep
+
+import crayons
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-js_name = 'my_todolist.js'
+js_name = "my_todolist.js"
 path_to_js = os.path.join(dir_path, js_name)
 
+
 def dump_todo_list_to_json():
-    print('dumping todo-list to JSON file')
-    with open(path_to_js, 'w') as f:
+    with open(path_to_js, "w") as f:
         json.dump(todos, f, indent=4)
+    print(crayons.yellow("\n[INFO] saved", bold=True))
 
 
 if not os.path.isfile(path_to_js):
-    print('not os.path.isfile(path_to_js)')
-    open(js_name, 'a').close()
+    print("not os.path.isfile(path_to_js)")
+    open(js_name, "a").close()
     todos = {}
-    todos['id'] = 0
-    todos['tasks'] = {}
+    todos["id"] = 0
+    todos["tasks"] = {}
     dump_todo_list_to_json()
 else:
-    with open(path_to_js, 'r') as f:
+    with open(path_to_js, "r") as f:
         todos = json.load(f)
 
 
 def get_id():
-    new_id = todos['id'] + 1
-    todos['id'] = new_id
+    new_id = todos["id"] + 1
+    todos["id"] = new_id
     return str(new_id)
 
 
 def list_all_tasks():
-    print('id:', todos['id'])
-    for id_key, task in todos['tasks'].items():
-        print('-'*40)
-        print(id_key, task['status'], task['text'])
+    print("id:", todos["id"])
+    for id_key, task in todos["tasks"].items():
+        print("-" * 40)
+        print(crayons.blue(id_key), task["status"], task["text"])
+        print(crayons.yellow(task["tags"]))
 
 
 def list_open_tasks():
     # print('id:', todos['id'])
-    for id_key, task in todos['tasks'].items():
-        if task['status'] == 'open':
-            print('-'*40)
-            print(id_key, task['status'], task['text'])
+    for id_key, task in todos["tasks"].items():
+        if task["status"] == "open":
+            # print("-" * 40)
+            print(crayons.blue(id_key), crayons.green(task["status"]), task["text"])
+
+            if len(task["comment"][0]) > 0:
+                for c in task["comment"]:
+                    print(' ' * 6, crayons.blue(c))
+
+            if len(task["tags"][0]) > 0:
+                print(' ' * 6, '- ', end='')
+                for t in task["tags"]:
+                    print(crayons.yellow(t), '- ', end='')
+                print('')
 
 
 def list_finished_tasks():
     # print('id:', todos['id'])
-    for id_key, task in todos['tasks'].items():
-        if task['status'] == 'finished':
-            print('-'*40)
-            print(id_key, task['status'], task['text'])
+    for id_key, task in todos["tasks"].items():
+        if task["status"] == "finished":
+            # print("-" * 40)
+            print(crayons.blue(id_key), crayons.blue(task["status"]), task["text"])
+
+            if len(task["comment"][0]) > 0:
+                for c in task["comment"]:
+                    print(' ' * 10, crayons.yellow(c))
+
+            if len(task["tags"][0]) > 0:
+                print(' ' * 10, '- ', end='')
+                for t in task["tags"]:
+                    print(crayons.yellow(t), '- ', end='')
+                print('')
 
 
 actions = {
-    'Add task' : 'n',
-    'Edit task' : 'e',
-    'Remove task' : 'r',
-    'Finish task' : 'f',
-    'List task' : 't',
-    'Cancel' : 'c'
-    }
+    "Add task": "n",
+    "Edit task": "e",
+    "Add comment task": "c",
+    "Remove task": "r",
+    "Finish task": "f",
+    "Reopen task": "o",
+    "List task": "t",
+    "Cancel": "y",
+    "Reset ALL": "resetall",
+}
+
+
 def list_actions():
+    length = 23
+    print('-' * length)
     for action, key in actions.items():
-        print(action, key)
-    print('')
+        x = length - len(action) - 12
+        print('|', crayons.yellow(action), ' ' * x, '[', crayons.yellow(key.upper()), ']', ' |')
+    print('-' * length)
+
+
+def extract_data(inp: str):
+    # print('extract_data', inp)
+    # ACTION
+    if inp == "resetall":
+        action = "resetall"
+    else:
+        action = inp[:1].lower()
+        if action not in actions.values():
+            # print("ERROR action")
+            action = None
+
+    # TAGS
+    tags = inp.split("*")[1:]
+
+    # input ohne TAGS
+    inp = inp.split("*")[0]
+    # TASK ID
+    try:
+        task_id = re.search(r"\d+", inp).group()
+    except AttributeError:
+        # print("ERROR task_id")
+        task_id = None
+
+    if task_id:
+        if task_id not in todos["tasks"].keys():
+            # print("ERROR task_id 2")
+            task_id = None
+
+    # TEXT
+    try:
+        text = inp.partition(task_id)[2].lstrip()
+    except TypeError:
+        # print("ERROR text")
+        try:
+            text = inp.partition('n')[2].lstrip()
+        except TypeError:
+            # print("ERROR text")
+            text = None
+    
+
+    # print(action, task_id, text)
+    return action, task_id, text, tags
 
 
 def _main():
     go = True
     while go:
+        # clear screen
+        os.system('cls')
+
+        print("\n", "#" * 10, " FINISHED ", "#" * 60, "\n", sep='')
+        list_finished_tasks()
+        print("\n", "#" * 10, " OPEN ", "#" * 64, "\n", sep='')
         list_open_tasks()
-        print('#'*80)
+        print("\n", "#" * 80, "\n", sep='')
 
         # list_actions()
-
-        action_input = input('Task:\t') or 0
+        print("EXIT Y")
+        action_input = input("Input:\t") or 0
+        # print('action_input', action_input)
 
         if action_input:
-            # print('input was:', action_input)
-            action = action_input[:1].lower()
-            # print('action', action)
-            # print('actions.values()')
+            action, task_id, text, tags = extract_data(action_input)
 
-            if action in actions.values():
-                if action == 'n':   # New entry
-                    print('Adding new task to ToDo-List')
-                    task_id = get_id()
-                    text = action_input[2:]
-                    todos['tasks'][task_id] = {'text' : text, 'status' : 'open'}
-                elif action == 'c': # Cancel program
-                    go = False
-                elif action == 't': # List all tasks
-                    print('list tasks')
-                else:
-                    task_id = re.search('\d+', action_input).group()
-                    if action == 'f':   # Set status to FINISH
-                        # task_id = action_input[2:]
-                        print('finishing task')
-                        todos['tasks'][task_id]['status'] = 'finished'
-                    elif action == 'r': # Remove existing task
-                        print('Removing task', todos[task_id]['text'])
-                        todos.pop(task_id, None)
-                    else:
-                    # try:
-                        _, task_id, text = re.split(" ", action_input, 2)
-                    # except:
+            if action == "resetall":
+                todos["id"] = 0
+                todos["tasks"] = {}
+                dump_todo_list_to_json()
+                continue
 
-                        if action == 'e':   # Edit existing task
-                            print('editing', todos['tasks'][task_id]['text'])
-                            todos['tasks'][task_id]['text'] = text
-            else:
-                print('wrong input')
+            if action == "n":                                               # New entry
+                task_id = get_id()
+                todos["tasks"][task_id] = {"text": text, "status": "open", "comment": "", "tags": tags}
+            elif action == "y":                                             # Cancel program
+                go = False
+            elif action == "t":                                             # List all tasks
+                print("list tasks")
+                sleep(3)
+            elif action == "f":                                             # Set status to FINISH
+                todos["tasks"][task_id]["status"] = "finished"
+            elif action == "o":                                             # Set status to OPEN
+                # todos["tasks"][task_id]["tags"].insert(0, "reopen")
+                todos["tasks"][task_id]["status"] = "open"
+            elif action == "r":                                             # Remove existing task
+                todos["tasks"].pop(task_id, None)
+            elif action == "e":                                             # Edit existing task
+                todos["tasks"][task_id]["text"] = text
+                todos["tasks"][task_id]["tags"] = tags
+            elif action == "c":  # Edit comment
+                comment_old = todos["tasks"][task_id]["comment"]
+                todos["tasks"][task_id]["comment"] = '\n'.join([comment_old, text])
 
             dump_todo_list_to_json()
 
-            # sys.stderr.write('The ID "%s" matches more than one task.\n' % e.prefix)
-            # sys.stderr.write('The ID "%s" does not match any task.\n' % e.prefix)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     _main()
-    print('Good Bye\n')
